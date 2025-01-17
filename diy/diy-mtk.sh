933f4a1343b86ff973b93126cdc39409299d8626
hong66489 <483242964@qq.com>
2025-01-18 01:09:12 +0800

    Update diy-mtk.sh

--- diy/diy-mtk.sh
+++ diy/diy-mtk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 # 设置默认密码
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
